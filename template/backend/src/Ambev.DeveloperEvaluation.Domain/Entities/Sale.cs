6a9a98042af17432c79becbad632d16c560f8c05
Alex Nalim <alex.nalim@patientmpower.com>
2025-02-27 15:12:52 -0300

    feat: Add Sale and SaleItem domain entities and Discount application service

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,23 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class Sale(Guid saleId, string saleNumber, DateTime saleDate, decimal totalAmount
+, bool isCancelled, List<SaleItem> items, Guid customerId, string customerName
+, Guid branchId, string branchName)
+{
+    public Guid SaleId { get; set; } = saleId;
+    public string SaleNumber { get;  set; } = saleNumber;
+    public DateTime SaleDate { get;  set; } = saleDate;
+    public decimal TotalAmount { get;  set; } = totalAmount;
+    public bool IsCancelled { get;  set; } = isCancelled;
+    public List<SaleItem> Items { get;  set; } = items;
+
+    public Guid CustomerId { get;  set; } = customerId;// External Identity
+    public string CustomerName { get;  set; } = customerName;// Denormalized data
+    public Guid BranchId { get;  set; } = branchId;// External Identity
+    public string BranchName { get;  set; } = branchName;// Denormalized data
+
+    public void CalculateTotal()
+    {
+        TotalAmount = Items.Sum(item => item.TotalPrice);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class SaleItem(
+     Guid saleItemId ,Guid saleId ,Guid productId    ,string productName    
+    ,int quantity ,decimal unitPrice  ,decimal discount      
+    ,decimal totalPrice    
+    ,bool isCancelled)      
+{
+    public Guid SaleItemId { get; set; } = saleItemId;
+    public Guid SaleId { get; set; } = saleId;
+    public Guid ProductId { get; set; } = productId; // External Identity
+    public string ProductName { get; set; } = productName; // Denormalized data
+    public int Quantity { get; set; } = quantity;
+    public decimal UnitPrice { get; set; } = unitPrice;
+    public decimal Discount { get; set; } = discount;
+    public decimal TotalPrice { get; set; } = totalPrice;
+    public bool IsCancelled { get; set; } = isCancelled;
+}
